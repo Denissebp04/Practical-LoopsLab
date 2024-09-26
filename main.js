f4986c952b2ddd9d25f63393d6a7b795cb6727d5
Denisse Belliard <153006924+Denissebp04@users.noreply.github.com>
2024-09-26 16:51:27 -0400

    Finished

--- main.js
+++ main.js
@@ -91,3 +91,33 @@ if (flag == 0)
 
 }
 
+
+// PART III
+
+//Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,
+//0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,
+//0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232
+
+let cel1 = 'Index'
+let cel2 = 'Mass (Kg)'
+let cel3 = 'Spring 1 (m)'
+let cel4 = 'Spring 2 (m)'
+
+console.log(cel1, cel2, cel3, cel4)
+
+let row1 = [0.00, 0.050, 0.050]
+let row2 = [0.49, 0.066, 0.066]
+let row3 = [0.98, 0.087, 0.080]
+let row4 = [1.47, 0.116, 0.108]
+let row5 = [1.96, 0.142, 0.138]
+let row6 = [2.45, 0.166, 0.158]
+let row7 = [2.94, 0.193, 0.174]
+let row8 = [3.43, 0.204, 0.192]
+let row9 = [3.92, 0.226, 0.205]
+let row10 = [4.41, 0.238, 0.232]
+
+const rows = [row1, row2, row3, row4, row5, row6, row7, row8, row9, row10];
+
+for (const c of rows) {
+	console.log(c);
+}
